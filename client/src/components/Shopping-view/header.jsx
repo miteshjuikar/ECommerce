943bf21e73738ece199576f4ee8500781991f805
miteshjuikar <106508740+miteshjuikar@users.noreply.github.com>
2024-10-08 13:00:54 +0530

    Shopping side navbar with logout functionality

--- client/src/components/Shopping-view/header.jsx
+++ client/src/components/Shopping-view/header.jsx
@@ -1,10 +1,124 @@
-import React from 'react'
+import { HousePlug, LogOut, ShoppingCart, UserCog } from 'lucide-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Label } from '../ui/label'
+import { shoppingViewHeaderMenuItems } from '../../config/index'
+import { Sheet } from '../ui/sheet'
+import { Button } from '../ui/button'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { useDispatch, useSelector } from 'react-redux'
+import { logoutUser } from '@/store/auth-slice'
+
+
+function MenuItems(){
+  const navigate = useNavigate();
+
+  
+  function handleNavigate(getCurrentMenuItem) {
+    navigate(getCurrentMenuItem.path);
+  }
+  
+  return(
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+      {
+        shoppingViewHeaderMenuItems.map((menuItem) => (
+          <Label
+          onClick={() => handleNavigate(menuItem)}
+          className="text-sm font-medium cursor-pointer"
+          key={menuItem.id}
+          >
+            {menuItem.label}
+          </Label>
+        ))
+      }
+    </nav>
+  )
+}
+
+function HeaderRightContent() {
+  const { user } = useSelector((state) => state.auth);
+  const [openCartSheet, setOpenCartSheet] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleLogout(){
+    dispatch(logoutUser());
+  }
+
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+      <Sheet open={openCartSheet} onOpenChange={() => setOpenCartSheet(false)}>
+        <Button
+          onClick={() => setOpenCartSheet(true)}
+          variant="outline"
+          size="icon"
+          className="relative"
+        >
+          <ShoppingCart className="w-6 h-6" />
+          <span className="absolute top-[-5px] right-[2px] font-bold text-sm">
+            {
+            // cartItems?.items?.length ||
+            0}
+          </span>
+          <span className="sr-only">User cart</span>
+        </Button>
+      
+      </Sheet>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar className="bg-black">
+            <AvatarFallback className="bg-black text-white font-extrabold">
+              {user?.userName[0].toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="right" className="w-56">
+          <DropdownMenuLabel>Logged in as {user?.userName}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={() => navigate("/shop/account")}>
+            <UserCog className="mr-2 h-4 w-4" />
+              Account
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleLogout}>
+            <LogOut className="mr-2 h-4 w-4" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
 
 function ShoppingHeader() {
   return (
-    <div>
-      ShoppingHeader
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">SBI</span>
+        </Link>
+        
+
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+
+        <div className="hidden lg:block">
+          <HeaderRightContent />
+        </div>
     </div>
+    </header>
   )
 }
 
